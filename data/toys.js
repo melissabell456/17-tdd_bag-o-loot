71cbc53c4dce711aed990ac7255a7d28b441b1bd
Melissa Bell <melissabell456@gmail.com>
2018-03-09 11:15:29 -0600

    file structure added and toy data file completed

---
+++ data/toys.js
@@ -0,0 +1,10 @@
+module.exports.toys = [
+  { name: 'Kylie', toy: 'stuffed animal', delivered: 0, naughty: 0},
+  { name: 'Niko', toy: 'coloring book', delivered: 0, naughty: 0},
+  { name: 'Niko', toy: 'ball', delivered: 0, naughty: 0},
+  { name: 'Jade', toy: 'rock', delivered: 0, naughty: 0},
+  { name: 'Jade', toy: 'shovels', delivered: 0, naughty: 0},
+  { name: 'Jade', toy: 'tennis ball', delivered: 0, naughty: 0},
+  { name: 'Laia', toy: 'plush carrot', delivered: 0, naughty: 0},
+  { name: 'Laia', toy: 'bone', delivered: 0, naughty: 0},
+]
